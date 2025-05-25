b31e1f70538f2f59f5b2962b6bea70ad07752338
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-05-25 18:27:00 +0000

    string format updated and casting user input to float instead of int

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,12 +2,12 @@
 income = input("Enter your monthly income: ")
 monthly_expenses = input("Enter your total monthly expenses: ")
 
-income = int(income)
-monthly_expenses = int(monthly_expenses)
+income = float(income)
+monthly_expenses = float(monthly_expenses)
 
 monthly_savings = income - monthly_expenses
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print(f"Your monthly savings are {monthly_savings}$.")
-print(f"Projected savings after one year, with interest, is: {round(projected_savings)}.")
+print(f"Your monthly savings are ${round(monthly_savings)}.")
+print(f"Projected savings after one year, with interest, is: ${round(projected_savings)}.")
