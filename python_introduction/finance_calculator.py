0a03105c7b695b42e297d4a39574f6fd697aa254
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-05-25 18:23:09 +0000

    rounding projected savings to the nearest integer instead of type castin

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -10,4 +10,4 @@ monthly_savings = income - monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print(f"Your monthly savings are {monthly_savings}$.")
-print(f"Projected savings after one year, with interest, is: {int(projected_savings)}.")
+print(f"Projected savings after one year, with interest, is: {round(projected_savings)}.")
