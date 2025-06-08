073c0c39bb401d9bef3dbf43e1dbce327cf5aa09
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-06-08 03:03:10 +0000

    switching from match case to if else

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,15 @@
 def perform_operation(num1, num2, operation):
-    match operation: 
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 != 0:
-                return num1 / num2
-            else:
-                print("Cannot divide by Zero")
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("Cannot divide by Zero")
+    else:
+        print("Invalid operation")
         
