56752cd9a62b87f94b5d1d40120ec3d97fd3839f
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-06-08 03:22:54 +0000

    rewriting user prompts for adding and removing

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,11 +13,11 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item_to_add = input("Add Item: ")
+            item_to_add = input("Enter the item to add: ")
             shopping_list.append(item_to_add)
         elif choice == '2':
             # Prompt for and remove an item
-            item_to_remove = input("Remove Item: ")
+            item_to_remove = input("Enter the item to remove: ")
             if item_to_remove in shopping_list:
                 shopping_list.remove(item_to_remove)
         elif choice == '3':
