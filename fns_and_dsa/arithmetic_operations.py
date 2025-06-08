2a423af85ca12ad1f75eea7d6df2603707a3b8c5
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-06-08 02:59:46 +0000

    perform simple math operations using the arithmetic function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1, num2, operation):
+    match operation: 
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                print("Cannot divide by Zero")
+        
