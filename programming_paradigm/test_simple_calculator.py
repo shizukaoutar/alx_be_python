bca76e025c1343daf7ff079baab00f2a9cc2bd7b
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-06-15 19:38:44 +0000

    updated the names of multiplication and division test functions

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,11 +14,11 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test subtraction method"""
         self.assertEqual(self.calc.subtract(2, 3), -1)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test multiplication method"""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
-    def test_divide(self):
+    def test_division(self):
         """Test division method"""
         self.assertEqual(self.calc.divide(14, 2), 7)
         self.assertEqual(self.calc.divide(14, 0), None)
