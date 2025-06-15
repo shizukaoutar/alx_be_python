ed5f5f7153ff392d569f72bca7e6d80100adce1d
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-06-15 19:03:00 +0000

    correcting string format

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def safe_divide(numerator, denominator):
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error: please enter numeric values only."
+        return "Error: Please enter numeric values only."
     else:
         return f"The result of the division is {result:.2}"
     
