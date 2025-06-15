1419db8b03fe5c1c1316e9447316c38c713f0f44
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-06-15 18:49:09 +0000

    float format to show 2 decimal places

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,6 +14,6 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
 
 
