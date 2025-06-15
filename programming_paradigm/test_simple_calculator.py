5d3d2164e5e71ed85b7bb9ade58b525fd902fa42
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-06-15 19:36:10 +0000

    working with test units for a simple calculator app

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,27 @@
+from simple_calculator import SimpleCalculator
+import unittest
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        """Set up the SimpleCaculator instance before each test"""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        """Test addition method"""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+    def test_subtract(self):
+        """Test subtraction method"""
+        self.assertEqual(self.calc.subtract(2, 3), -1)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+    def test_multiply(self):
+        """Test multiplication method"""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+    def test_divide(self):
+        """Test division method"""
+        self.assertEqual(self.calc.divide(14, 2), 7)
+        self.assertEqual(self.calc.divide(14, 0), None)
+
+
+
